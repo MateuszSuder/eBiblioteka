6bef7bb0d662bc9919d2c9df8f5ee4a662ef8f96
Mateusz Suder <matisu999@gmail.com>
2022-12-08 18:42:49 +0100

    Add init

---
+++ init.bat
@@ -0,0 +1,7 @@
+IF not exist auth-service (
+    git clone https://github.com/MateuszSuder/eBiblioteka-auth auth-service
+)
+
+IF not exist user-service (
+    git clone https://github.com/MateuszSuder/eBiblioteka-user user-service
+)
