4c16392ce7147a0b98a5f4da698b4d1c497eb90e
Mateusz Suder <matisu999@gmail.com>
2022-12-08 18:43:42 +0100

    Change init

--- init.bat
+++ init.bat
@@ -5,3 +5,15 @@ IF not exist auth-service (
 IF not exist user-service (
     git clone https://github.com/MateuszSuder/eBiblioteka-user user-service
 )
+
+IF not exist book-service (
+    git clone https://github.com/MateuszSuder/eBiblioteka-book book-service
+)
+
+IF not exist reservation-service (
+    git clone https://github.com/MateuszSuder/eBiblioteka-reservation reservation-service
+)
+
+IF not exist notification-service (
+    git clone https://github.com/MateuszSuder/eBiblioteka-notification notification-service
+)
